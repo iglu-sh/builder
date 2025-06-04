9ca08af3986c71e199fdf907c2bccccc67c7e19f
Sven Ziegler <43136984+Svenum@users.noreply.github.com>
2025-06-04 13:49:03 +0200

    add logger + limit number of concurrent builds to 1 (#8)
    * add logger
    
    * update nodeModule hash
    
    * add debug logging

--- index.ts
+++ index.ts
@@ -1,23 +1,36 @@
-import type {Request, Response} from 'express'
+import type {NextFunction, Request, Response} from 'express'
 import raw from 'express'
 import createRouter from "express-file-routing"
 import ws from "express-ws"
+import {Logger} from './utils/logger'
 
+// Configure Logger
+const log = new Logger()
+log.setJsonLogging(process.env.JSON_LOGGING && process.env.JSON_LOGGING == 'true' ? true : false)
+log.setLogLevel(process.env.LOG_LEVEL ? process.env.LOG_LEVEL : "INFO")
+
+// Create webserver
 const express = require("express")
 const app = express()
 const expressWs = require("express-ws")(app)
 
 app.use(raw())
 
+app.use((req:Request, res:Response, next:NextFunction) => {
+
+  //Log the request                                                
+  res.on('finish', ()=>{                                   
+      //Log the response                                   
+      Logger.logResponse(req.url, req.method, res.statusCode)                                                                  
+  })
+
+  next()
+})
+
 await createRouter(app, {
   additionalMethods: [ "ws" ]
 })
 
-app.use((req:Request, res:Response) => {
-  console.log(req.url)
-  console.log(req.method)
-  console.log(req.headers)
-})
-
+Logger.info("Server ist listening on http://localhost:3000")
 app.listen(3000)
 
