9d5dc27c6a83011c21c22330253fe0597af97a09
Sven Ziegler <43136984+Svenum@users.noreply.github.com>
2025-07-16 10:11:58 +0200

    add script (#19)
    * add script
    
    * nixfmt

--- flake.nix
+++ flake.nix
@@ -2,10 +2,8 @@
   description = "A very basic flake";
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
-    utils.url =
-      "github:gytis-ivaskevicius/flake-utils-plus?ref=afcb15b845e74ac5e998358709b2b5fe42a948d1";
-    iglu-flake.url =
-      "github:iglu-api/flake?ref=dc52f8b84110aea5eae65b8e0cdd2a72c885c547";
+    utils.url = "github:gytis-ivaskevicius/flake-utils-plus?ref=afcb15b845e74ac5e998358709b2b5fe42a948d1";
+    iglu-flake.url = "github:iglu-api/flake?ref=4aae76f770fe362851673e14fa18bf98be788bf4";
   };
 
   # deadnix: skip
