51cfaa9a886d5f0df7c0858df5f1e2f7eb025c1c
Sven Ziegler <43136984+Svenum@users.noreply.github.com>
2025-11-18 09:48:29 +0100

    Enable cross-compiling (#40)
    * first steps
    
    * configure binfmt the right way
    
    * add more nix deps
    
    * disable sandbox
    
    * fix formating

--- nix/pkgs/iglu-builder-docker/default.nix
+++ nix/pkgs/iglu-builder-docker/default.nix
@@ -1,20 +1,30 @@
 { dockerTools
 , writeTextFile
 , iglu
-, bash
+, bashInteractive
+, gitMinimal
+, gnutar
+, gzip
+, openssh
+, xz
+, iana-etc
 , buildEnv
 , tini
 , nix
 , cachix
 , stdenv
-, toybox
+, busybox
 , man
+, wrapQemuBinfmtP
+, qemu-user
 }:
 
 let
   nixosVersion = "25.05";
   archType =
     if (stdenv.hostPlatform.system == "x86_64-linux") then "amd64" else "arm64";
+  ccType =
+    if (stdenv.hostPlatform.system == "x86_64-linux") then "aarch64" else "x86_64";
 
   buildUsers = [ "nixbld:x:30000:30000:Nix build user 0:/var/empty:/noshell" ] ++ (builtins.genList
     (i:
@@ -30,6 +40,8 @@ let
         "nixbld${toString i}"
       ) 32)))
   ];
+
+  binfmtQemu = wrapQemuBinfmtP "qemu-${ccType}-binfmt-P" "${qemu-user}/bin/qemu-${ccType}";
 in
 dockerTools.buildImageWithNixDb {
   name = "iglu-builder";
@@ -39,9 +51,20 @@ dockerTools.buildImageWithNixDb {
     name = "image-root";
     paths = with dockerTools; [
       iglu.iglu-builder
-      bash
-      toybox
+      iglu.enable-cc
+      binfmtQemu
+      bashInteractive
+      busybox
+
+      # runtime dependencies of nix
       nix
+      gitMinimal
+      gnutar
+      gzip
+      openssh
+      xz
+      iana-etc
+
       cachix
       tini
       caCertificates
@@ -54,9 +77,20 @@ dockerTools.buildImageWithNixDb {
         name = "nix.conf";
         destination = "/etc/nix/nix.conf";
         text = ''
+          # Nix
           accept-flake-config = true
           experimental-features = nix-command flakes
           max-jobs = auto
+          trusted-users = root
+
+          # Crosscompiling
+          extra-platforms = ${ccType}-linux i686-linux
+          extra-sandbox-paths = /run/binfmt ${binfmtQemu}
+          system-features = nixos-test benchmark big-parallel kvm
+
+          # Sandboxing does currently not work with cross-compiling
+          sandbox = false
+          sandbox-fallback = true
         '';
       })
     ];
@@ -73,18 +107,27 @@ dockerTools.buildImageWithNixDb {
 
     # create root Home
     mkdir -vp root
+
+    # Create /run/binfmt
+    mkdir -p run/binfmt
+    ln -s ../../bin/qemu-${ccType}-binfmt-P run/binfmt/${ccType}-linux
   '';
 
   config = {
     Env = [
+      # NIX ENVs
       "NIX_BUILD_SHELL=/bin/bash"
       "NIX_PATH=nixpkgs=https://github.com/NixOS/nixpkgs/archive/refs/tags/${nixosVersion}.tar.gz"
-      "PATH=/usr/bin:/bin"
+      "BASH_ENV=/etc/profile.d/nix.sh"
+      "ENV=/etc/profile.d/nix.sh"
+
+      # Other
+      "PATH=/root/.nix-profile/bin:/usr/bin:/bin"
       "PAGER=cat"
       "USER=root"
     ];
     ExposedPorts = { "3000/tcp" = { }; };
     Cmd = [ "/bin/iglu-builder" ];
-    Entrypoint = [ "/bin/tini" ];
+    Entrypoint = [ "/bin/entrypoint.sh" ];
   };
 }
